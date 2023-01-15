fa34c0be2653af3f62f0d403e1f58336716ceea3
Piotr Piasecki <101048348+piaseckipiotr@users.noreply.github.com>
2023-01-15 10:05:31 +0100

    Update zad02.py
    Added randint to function for more characters.

--- zadnielab03/zad02.py
+++ zadnielab03/zad02.py
@@ -4,11 +4,11 @@ import random
 
 def create_secure_password():
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = random.choices(characters, k=8)
+    password = random.choices(characters, k=random.randint(8, 18))
     password += random.choices(string.ascii_uppercase, k=2)
     password += random.choices(string.ascii_lowercase, k=2)
-    password += random.choices(string.digits, k=2)
-    password += random.choices(string.punctuation, k=2)
+    password += random.choices(string.digits, k=1)
+    password += random.choices(string.punctuation, k=1)
     random.shuffle(password)
     print("".join(password))
 
